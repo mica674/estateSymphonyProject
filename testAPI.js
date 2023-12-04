51abf9971301a54f8549711f6e9d7faa41873b4b
Mickaël <mica.fournier@hotmail.fr>
2023-12-04 16:05:25 +0100

    first commit2

---
+++ testAPI.js
@@ -0,0 +1,19 @@
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-type', 'application/json');
+//     res.send('Hello world\n');
+// })
+
+// server.listen(port, hostname, () => {
+//     console.log(`Serveur démarrer à l'adresse http://${hostname}:${port}/`);
+// })
+
+let db = {}
+
+db.get
