702dcdf23544a8f5b9ec696fe2ab4665036c1358
Mickaël <mica.fournier@hotmail.fr>
2023-12-05 09:16:07 +0100

    Seeder example + CMD migration & seeders en txt

---
+++ seeders/ExampleSeeder-demo-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [{
+      firstname:'John',
+      lastname: 'Doe',
+      email : 'example@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+    
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Users', null, {})
+    
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
