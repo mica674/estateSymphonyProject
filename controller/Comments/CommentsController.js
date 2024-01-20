14e5df3cc4a6669c124546869fdde49bf4d2d679
Mickaël <mica.fournier@hotmail.fr>
2024-01-20 20:51:00 +0100

    [SWAGGER]  Comments TESTED

--- controller/Comments/CommentsController.js
+++ controller/Comments/CommentsController.js
@@ -1,29 +1,36 @@
 const db = require('../../models/index.js');
 const commentsTable = db['Comments'];
+const usersTable = db['User'];
 
-const getComment = async (req,res)=>{
+const getComment = async (req, res) => {
     try {
         const comment = await commentsTable.findByPk(req.params.id);
+        if (comment !== null) {
+            res.status(200).send({
+                message: `Comment id : ${comment.id}`,
+                data: comment
+            })
+        } else {
+            res.status(404).send({
+                message: 'Comment was not found'
+            })
+        }
 
-        res.status(200).send({
-            message: `Commentaire ${comment.id}`,
-            data:comment
-        })
     } catch (error) {
         res.status(400).send({
             message: 'Erreur de synthaxe de la requête.',
             error: error.message
-        })  
+        })
     }
 }
 
-const getComments = async (req,res)=>{
+const getComments = async (req, res) => {
     try {
         const comments = await commentsTable.findAll();
 
         res.status(200).send({
-            message : 'select all',
-            data:comments
+            message: 'select all of comments',
+            data: comments
         })
 
     } catch (error) {
@@ -34,56 +41,27 @@ const getComments = async (req,res)=>{
     }
 }
 
-const createComment = async (req,res)=>{
+const createComment = async (req, res) => {
     try {
-        let data = {...req.body};
-        const newComments = await commentsTable.create(data);
-        
-        res.status(200).send({
-            message : 'Commentaire créé',
-            data: newComments
-        })
-    } catch (error) {
-        res.status(400).send({
-            message: 'Erreur de synthaxe de la requête.',
-            error: error.message
-        })
-    }
-}
-
-const modifyComment = async (req,res)=>{
-    try {
-        const {comment} = req.body;
-        const idComments = req.params.id;
-        const updateComments = await commentsTable.update(
-            {comment : comment},
-            {where :{
-                    id : idComments
-                }
-            })
-            if(updateComments[0] == 1){
+        let data = { ...req.body };
+        let idUserFound = await usersTable.findOne({
+            where: { id: data.idUsers }
+        });
+        if (idUserFound !== null) {
+            const newComments = await commentsTable.create(data);
+            if (newComments !== null) {
                 res.status(200).send({
-                    message : 'Commentaire modifié'
+                    message: 'Comment created',
+                    data: newComments
+                })
+            } else {
+                res.status(400).send({
+                    message: 'Comment was not created'
                 })
             }
-    } catch (error) {
-        res.status(400).send({
-            message: 'Erreur de synthaxe de la requête.',
-            error: error.message
-        })
-    }
-}
-
-const deleteComment =async (req, res)=>{
-    try {
-        const idComment = req.params.id;
-        const deletedComment = await commentsTable.destroy({
-            where  : {id : idComment}
-        })
-        console.log(deletedComment);
-        if (deletedComment == 1) {
-            res.status(200).send({
-                message: 'Commentaire supprimé'
+        } else {
+            res.status(404).send({
+                message: 'idUsers not existed'
             })
         }
     } catch (error) {
@@ -94,4 +72,57 @@ const deleteComment =async (req, res)=>{
     }
 }
 
-module.exports = {getComment, getComments, createComment, modifyComment, deleteComment}
+const modifyComment = async (req, res) => {
+    try {
+        const { comment } = req.body;
+        const idComments = req.params.id;
+        const updateComments = await commentsTable.update(
+            { comment: comment },
+            {
+                where: {
+                    id: idComments
+                }
+            })
+        if (updateComments[0] == 1) {
+            res.status(200).send({
+                message: 'Comment was updated'
+            });
+        }
+        else {
+            res.status(400).send({
+                message: 'Comment was not updated'
+            })
+        }
+    } catch (error) {
+        res.status(400).send({
+            message: 'Erreur de synthaxe de la requête.',
+            error: error.message
+        })
+    }
+}
+
+const deleteComment = async (req, res) => {
+    try {
+        const idComment = req.params.id;
+        const deletedComment = await commentsTable.destroy({
+            where: { id: idComment }
+        })
+        console.log(deletedComment);
+        if (deletedComment == 1) {
+            res.status(200).send({
+                message: 'Comment was deleted'
+            })
+        } else {
+            res.status(400).send({
+                message: 'Comment was not deleted'
+            })
+        }
+    } catch (error) {
+        res.status(400).send({
+            message: 'Erreur de synthaxe de la requête.',
+            error: error.message
+        })
+    }
+}
+
+module.exports = { getComment, getComments, createComment, modifyComment, deleteComment }
--- models/comments.js
+++ models/comments.js
@@ -4,16 +4,13 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Comments extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
-      Comments.belongsTo(models.User);
+      Comments.belongsTo(models.User, {
+        foreignKey: 'idUsers',
+        as: 'userComment'
+      });
     }
-  }
+  };
   Comments.init({
     comment: DataTypes.STRING,
     idUsers: DataTypes.INTEGER
