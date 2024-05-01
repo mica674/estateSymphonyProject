85e53f56cce389dca52b8448a7e3fe8c580848bd
Mickaël <mica.fournier@hotmail.fr>
2024-05-01 09:27:01 +0200

    [Continuous Improvement] Factorisation des messages d'erreurs dans un fichier de constantes

---
+++ config/Constants.js
@@ -0,0 +1,21 @@
+//Messages d'erreurs
+//Districts
+const DistrictNoFound = 'Le district n\'a pas été trouvé';
+const NoDistrictFound = 'Aucun District trouvé';
+const DistrictCreated = 'District créé';
+const DistrictNotCreated = 'District n\'a pas été créé';
+const DistrictUpdated = 'District modifié';
+const DistrictNotUpdated = 'District n\'a pas été modifié';
+const DistrictDeleted = 'District supprimé';
+const DistrictNotDeleted = 'District n\'a pas été supprimé';
+
+module.exports = {
+    DistrictNoFound,
+    NoDistrictFound,
+    DistrictCreated,
+    DistrictNotCreated,
+    DistrictUpdated,
+    DistrictNotUpdated,
+    DistrictDeleted,
+    DistrictNotDeleted,
+}
