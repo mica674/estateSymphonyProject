01784eef74f8b15353af4c0d728df9493f1925ee
Mickaël <mica.fournier@hotmail.fr>
2024-05-13 10:24:36 +0200

    [HELPERS] Constants

--- config/Constants.js
+++ config/Constants.js
@@ -1,15 +1,214 @@
 //Messages d'erreurs
-//Districts
-const DistrictNoFound = 'Le district n\'a pas été trouvé';
-const NoDistrictFound = 'Aucun District trouvé';
-const DistrictCreated = 'District créé';
-const DistrictNotCreated = 'District n\'a pas été créé';
-const DistrictUpdated = 'District modifié';
-const DistrictNotUpdated = 'District n\'a pas été modifié';
-const DistrictDeleted = 'District supprimé';
-const DistrictNotDeleted = 'District n\'a pas été supprimé';
+//GLOBAL
+const SyntaxErrorMessage = 'Erreur de synthaxe de la requête.';
+
+//#region Agendas
+const AgendaNoFound = 'L\'agenda n\'a pas été trouvé';
+const NoAgendaFound = 'Aucun agenda trouvé';
+const AgendaCreated = 'Agenda créé';
+const AgendaNotCreated = 'L\'agenda n\'a pas été créé';
+const AgendaUpdated = 'Agenda modifié';
+const AgendaNotUpdated = 'L\'agenda n\'a pas été modifié';
+const AgendaDeleted = 'Agenda supprimé';
+const AgendaNotDeleted = 'L\'agenda n\'a pas été supprimé';
+//#endregion Agendas
+
+//#region ClientFolders
+const ClientFolderNoFound = 'Le dossier client n\'a pas été trouvé';
+const NoClientFolderFound = 'Aucun dossier client trouvé';
+const ClientFolderCreated = 'Dossier client créé';
+const ClientFolderNotCreated = 'Le dossier client n\'a pas été créé';
+const ClientFolderUpdated = 'Dossier client modifié';
+const ClientFolderNotUpdated = 'Le dossier client n\'a pas été modifié';
+const ClientFolderDeleted = 'Dossier client supprimé';
+const ClientFolderNotDeleted = 'Le dossier client n\'a pas été supprimé';
+//#endregion ClientFolders
+
+//#region Comments
+const CommentNoFound = 'Le commentaire n\'a pas été trouvé';
+const NoCommentFound = 'Aucun commentaire trouvé';
+const CommentCreated = 'Commentaire créé';
+const CommentNotCreated = 'Le commentaire n\'a pas été créé';
+const CommentUpdated = 'Commentaire modifié';
+const CommentNotUpdated = 'Le commentaire n\'a pas été modifié';
+const CommentDeleted = 'Commentaire supprimé';
+const CommentNotDeleted = 'Le commentaire n\'a pas été supprimé';
+//#endregion Comments
+
+//#region Districts
+const DistrictNoFound = 'Le quartier n\'a pas été trouvé';
+const NoDistrictFound = 'Aucun quartier trouvé';
+const DistrictCreated = 'Quartier créé';
+const DistrictNotCreated = 'Le quartier n\'a pas été créé';
+const DistrictUpdated = 'Quartier modifié';
+const DistrictNotUpdated = 'Le quartier n\'a pas été modifié';
+const DistrictDeleted = 'Quartier supprimé';
+const DistrictNotDeleted = 'Le quartier n\'a pas été supprimé';
+//#endregion Districts
+
+//#region Employees
+const EmployeeNoFound = 'L\'employé n\'a pas été trouvé';
+const NoEmployeeFound = 'Aucun employé trouvé';
+const EmployeeCreated = 'Employé créé';
+const EmployeeNotCreated = 'L\'employé n\'a pas été créé';
+const EmployeeUpdated = 'Employé modifié';
+const EmployeeNotUpdated = 'L\'employé n\'a pas été modifié';
+const EmployeeDeleted = 'Employé supprimé';
+const EmployeeNotDeleted = 'L\'employé n\'a pas été supprimé';
+//#endregion Employees
+
+//#region EmployeesDistricts
+const EmployeeDistrictNoFound = 'L\'employé-Quartier n\'a pas été trouvé';
+const NoEmployeeDistrictFound = 'Aucun employé-Quartier trouvé';
+const EmployeeAndDistritNotFound = 'District ET Employé n\'ont pas été trouvés'
+const EmployeeDistrictCreated = 'Employé-Quartier créé';
+const EmployeeDistrictNotCreated = 'L\'employé-Quartier n\'a pas été créé';
+const EmployeeDistrictUpdated = 'Employé-Quartier modifié';
+const EmployeeDistrictNotUpdated = 'L\'employé-Quartier n\'a pas été modifié';
+const EmployeeDistrictDeleted = 'Employé-Quartier supprimé';
+const EmployeeDistrictNotDeleted = 'L\'employé-Quartier n\'a pas été supprimé';
+//#endregion EmployeesDistricts
+
+//#region Estimations
+const EstimationNoFound = 'L\'estimation n\'a pas été trouvée';
+const NoEstimationFound = 'Aucune estimation trouvée';
+const EstimationCreated = 'Estimation créée';
+const EstimationNotCreated = 'L\'estimation n\'a pas été créée';
+const EstimationUpdated = 'Estimation modifiée';
+const EstimationNotUpdated = 'L\'estimation n\'a pas été modifiée';
+const EstimationDeleted = 'Estimation supprimée';
+const EstimationNotDeleted = 'L\'estimation n\'a pas été supprimée';
+//#endregion Estimations
+
+//#region Favorites
+const FavoriteNoFound = 'Le favori n\'a pas été trouvé';
+const NoFavoriteFound = 'Aucun favori trouvé';
+const FavoriteCreated = 'Favori créé';
+const FavoriteNotCreated = 'Le favori n\'a pas été créé';
+const FavoriteUpdated = 'Favori modifié';
+const FavoriteNotUpdated = 'Le favori n\'a pas été modifié';
+const FavoriteDeleted = 'Favori supprimé';
+const FavoriteNotDeleted = 'Le favori n\'a pas été supprimé';
+//#endregion Favorites
+
+//#region Histories
+const HistoryNoFound = 'L\'historique n\'a pas été trouvé';
+const NoHistoryFound = 'Aucun historique trouvé';
+const HistoryCreated = 'Historique créé';
+const HistoryNotCreated = 'L\'historique n\'a pas été créé';
+const HistoryUpdated = 'Historique modifié';
+const HistoryNotUpdated = 'L\'historique n\'a pas été modifié';
+const HistoryDeleted = 'Historique supprimé';
+const HistoryNotDeleted = 'L\'historique n\'a pas été supprimé';
+//#endregion Histories
+
+//#region Properties
+const PropertyNoFound = 'La propriété n\'a pas été trouvée';
+const NoPropertyFound = 'Aucune propriété trouvée';
+const PropertyCreated = 'Propriété créée';
+const PropertyNotCreated = 'La propriété n\'a pas été créée';
+const PropertyUpdated = 'Propriété modifiée';
+const PropertyNotUpdated = 'La propriété n\'a pas été modifiée';
+const PropertyDeleted = 'Propriété supprimée';
+const PropertyNotDeleted = 'La propriété n\'a pas été supprimée';
+//#endregion Properties
+
+//#region PropertiesFolders
+const PropertyFolderNoFound = 'Le dossier de la propriété n\'a pas été trouvé';
+const NoPropertyFolderFound = 'Aucun dossier de propriété trouvé';
+const PropertyFolderCreated = 'Dossier de propriété créé';
+const PropertyFolderNotCreated = 'Le dossier de la propriété n\'a pas été créé';
+const PropertyFolderUpdated = 'Dossier de propriété modifié';
+const PropertyFolderNotUpdated = 'Le dossier de la propriété n\'a pas été modifié';
+const PropertyFolderDeleted = 'Dossier de propriété supprimé';
+const PropertyFolderNotDeleted = 'Le dossier de la propriété n\'a pas été supprimé';
+//#endregion PropertiesFolders
+
+//#region Roles
+const RoleNoFound = 'Le role n\'a pas été trouvé';
+const NoRoleFound = 'Aucun role trouvé';
+const RoleCreated = 'Role créé';
+const RoleNotCreated = 'Le role n\'a pas été créé';
+const RoleUpdated = 'Role modifié';
+const RoleNotUpdated = 'Le role n\'a pas été modifié';
+const RoleDeleted = 'Role supprimé';
+const RoleNotDeleted = 'Le role n\'a pas été supprimé';
+//#endregion Roles
+
+//#region Statuses
+const StatusNoFound = 'Le status n\'a pas été trouvé';
+const NoStatusFound = 'Aucun status trouvé';
+const StatusCreated = 'Status créé';
+const StatusNotCreated = 'Le status n\'a pas été créé';
+const StatusUpdated = 'Status modifié';
+const StatusNotUpdated = 'Le status n\'a pas été modifié';
+const StatusDeleted = 'Status supprimé';
+const StatusNotDeleted = 'Le status n\'a pas été supprimé';
+//#endregion Statuses
+
+//#region Users
+const UserNoFound = 'L\'utilisateur n\'a pas été trouvé';
+const NoUserFound = 'Aucun utilisateur trouvé';
+const UserCreated = 'Utilisateur créé';
+const UserNotCreated = 'L\'utilisateur n\'a pas été créé';
+const UserUpdatedInfos = 'Informations modifiées';
+const UserNotUpdatedInfos = 'Les informations n\'ont pas été modifiées';
+const UserUpdatedPwd = 'Mot de passe modifié';
+const UserNotUpdatedPwd = 'Le mot de passe n\'a pas été modifié';
+const UserDeleted = 'Utilisateur supprimé';
+const UserNotDeleted = 'L\'utilisateur n\'a pas été supprimé';
+const UserEmployeeNoFound = 'Utilisateur ET Employé n\'ont pas été trouvés';
+const EmailAlreadyUsed = 'L\'Adresse email est déjà utilisé';
+const EmailUpdated = 'L\'adresse email a été modifiée'
+const EmailNotUpdated = 'L\'adresse email n\'a pas été modifiée';
+const PwdOldInvalid = 'Ancien mot de passe incorrect';
+const CredentialsFailed = 'Adresse email et/ou mot de passe incorrect(s)';
+const InvalidToken = 'Token invalide';
+//#endregion Users
+
+//#region UsersProperties
+const UserPropertyNoFound = 'La propriété-utilisateur n\'a pas été trouvée';
+const NoUserPropertyFound = 'Aucune propriété-utilisateur trouvée';
+const UserPropertyCreated = 'Propriété-utilisateur créée';
+const UserPropertyNotCreated = 'La propriété-utilisateur n\'a pas été créée';
+const UserPropertyUpdated = 'Propriété-utilisateur modifiée';
+const UserPropertyNotUpdated = 'La propriété-utilisateur n\'a pas été modifiée';
+const UserPropertyDeleted = 'Propriété-utilisateur supprimée';
+const UserPropertyNotDeleted = 'La propriété-utilisateur n\'a pas été supprimée';
+//#endregion UsersProperties
+
+
 
 module.exports = {
+    SyntaxErrorMessage,
+
+    AgendaNoFound,
+    NoAgendaFound,
+    AgendaCreated,
+    AgendaNotCreated,
+    AgendaUpdated,
+    AgendaNotUpdated,
+    AgendaDeleted,
+    AgendaNotDeleted,
+
+    ClientFolderNoFound,
+    NoClientFolderFound,
+    ClientFolderCreated,
+    ClientFolderNotCreated,
+    ClientFolderUpdated,
+    ClientFolderNotUpdated,
+    ClientFolderDeleted,
+    ClientFolderNotDeleted,
+
+    CommentNoFound,
+    NoCommentFound,
+    CommentCreated,
+    CommentNotCreated,
+    CommentUpdated,
+    CommentNotUpdated,
+    CommentDeleted,
+    CommentNotDeleted,
+
     DistrictNoFound,
     NoDistrictFound,
     DistrictCreated,
@@ -18,4 +217,115 @@ module.exports = {
     DistrictNotUpdated,
     DistrictDeleted,
     DistrictNotDeleted,
+
+    EmployeeNoFound,
+    NoEmployeeFound,
+    EmployeeCreated,
+    EmployeeNotCreated,
+    EmployeeUpdated,
+    EmployeeNotUpdated,
+    EmployeeDeleted,
+    EmployeeNotDeleted,
+
+    EmployeeDistrictNoFound,
+    NoEmployeeDistrictFound,
+    EmployeeAndDistritNotFound,
+    EmployeeDistrictCreated,
+    EmployeeDistrictNotCreated,
+    EmployeeDistrictUpdated,
+    EmployeeDistrictNotUpdated,
+    EmployeeDistrictDeleted,
+    EmployeeDistrictNotDeleted,
+
+    EstimationNoFound,
+    NoEstimationFound,
+    EstimationCreated,
+    EstimationNotCreated,
+    EstimationUpdated,
+    EstimationNotUpdated,
+    EstimationDeleted,
+    EstimationNotDeleted,
+
+    FavoriteNoFound,
+    NoFavoriteFound,
+    FavoriteCreated,
+    FavoriteNotCreated,
+    FavoriteUpdated,
+    FavoriteNotUpdated,
+    FavoriteDeleted,
+    FavoriteNotDeleted,
+
+    HistoryNoFound,
+    NoHistoryFound,
+    HistoryCreated,
+    HistoryNotCreated,
+    HistoryUpdated,
+    HistoryNotUpdated,
+    HistoryDeleted,
+    HistoryNotDeleted,
+
+    PropertyNoFound,
+    NoPropertyFound,
+    PropertyCreated,
+    PropertyNotCreated,
+    PropertyUpdated,
+    PropertyNotUpdated,
+    PropertyDeleted,
+    PropertyNotDeleted,
+
+    PropertyFolderNoFound,
+    NoPropertyFolderFound,
+    PropertyFolderCreated,
+    PropertyFolderNotCreated,
+    PropertyFolderUpdated,
+    PropertyFolderNotUpdated,
+    PropertyFolderDeleted,
+    PropertyFolderNotDeleted,
+
+    RoleNoFound,
+    NoRoleFound,
+    RoleCreated,
+    RoleNotCreated,
+    RoleUpdated,
+    RoleNotUpdated,
+    RoleDeleted,
+    RoleNotDeleted,
+
+    StatusNoFound,
+    NoStatusFound,
+    StatusCreated,
+    StatusNotCreated,
+    StatusUpdated,
+    StatusNotUpdated,
+    StatusDeleted,
+    StatusNotDeleted,
+
+    UserNoFound,
+    NoUserFound,
+    UserCreated,
+    UserNotCreated,
+    UserUpdatedInfos,
+    UserNotUpdatedInfos,
+    UserDeleted,
+    UserNotDeleted,
+
+    UserPropertyNoFound,
+    NoUserPropertyFound,
+    UserPropertyCreated,
+    UserPropertyNotCreated,
+    UserPropertyUpdated,
+    UserPropertyNotUpdated,
+    UserPropertyDeleted,
+    UserPropertyNotDeleted,
+    UserEmployeeNoFound,
+    EmailAlreadyUsed,
+    CredentialsFailed,
+    InvalidToken,
+    UserUpdatedPwd,
+    UserNotUpdatedPwd,
+    EmailUpdated,
+    EmailNotUpdated,
+    PwdOldInvalid,
+
+
 }
