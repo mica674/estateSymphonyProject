51162b0e9e0553a9e6941d94d1a3278936f07cd5
Mickaël <mica.fournier@hotmail.fr>
2024-11-15 11:34:54 +0100

    [Deploy] seed & migrate scripts added to predeploy

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const router = require('./routes/route')
 
 
-const hostname = '0.0.0.0';
-const port = 3000;
+const HOST = '0.0.0.0';
+const PORT = process.env.PORT || 3000; // Utilisation de la variable d'environnement
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,9 +15,17 @@ app.use(cors());
 app.use(router);
 app.use(express.static('public'));
 
-app.listen(port, hostname, () => {
-    console.log(`Serveur démarrer à l'adresse http://${hostname}:${port}/`);
-})
+//Health Check
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
+setTimeout(() => {
+
+    app.listen(PORT, HOST, () => {
+        console.log(`Serveur démarrer à l'adresse http://${HOST}:${PORT}/`);
+    })
+}, 1000);// Délai d'1 seconde avant de démarrer l'écoute
 
 //test email
 // require ('./NodeMailerTest.js');
