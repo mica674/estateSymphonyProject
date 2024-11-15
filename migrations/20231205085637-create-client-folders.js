1427a5af7b1fd890f1264a8719dc6c8effc10568
Mickaël <mica.fournier@hotmail.fr>
2024-11-15 16:07:30 +0100

    [Deploy] ClientFolders dropTable fixed

--- migrations/20231205085637-create-client-folders.js
+++ migrations/20231205085637-create-client-folders.js
@@ -35,6 +35,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('clientFolders');
+    await queryInterface.dropTable('ClientFolders');
   }
 };
