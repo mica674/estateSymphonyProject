d14cd79c6bb5b463341a36901e8e3cd10df3dfbd
Mickaël <mica.fournier@hotmail.fr>
2024-11-18 14:40:18 +0100

    [Seeder] Agendas & Employees disabled

--- seeders/DISABLED_20240108123346-Employees.js
+++ seeders/DISABLED_20240108123346-Employees.js
@@ -3,18 +3,18 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Employees', [
-      {
-        descriptions: 'Employé du mois',
-        name: 'Alain',
-        createdAt: new Date(),
-      },
-      {
-        descriptions: 'Employé de base',
-        name: 'Benoit',
-        createdAt: new Date(),
-      }
-    ], {});
+    // return queryInterface.bulkInsert('Employees', [
+    //   {
+    //     descriptions: 'Employé du mois',
+    //     name: 'Alain',
+    //     createdAt: new Date(),
+    //   },
+    //   {
+    //     descriptions: 'Employé de base',
+    //     name: 'Benoit',
+    //     createdAt: new Date(),
+    //   }
+    // ], {});
   },
 
   async down(queryInterface, Sequelize) {
--- seeders/DISABLED_20240108123421-Agendas.js
+++ seeders/DISABLED_20240108123421-Agendas.js
@@ -2,19 +2,19 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Agendas', [
-      {
-        date : '2024-12-26',
-        visitInformations: 'Ceci est une description',
-        userId: 1, 
-        idEmployee: 2,
-        createdAt : new Date(),
-      }
-    ], {});
+  async up(queryInterface, Sequelize) {
+    // return queryInterface.bulkInsert('Agendas', [
+    //   {
+    //     date : '2024-12-26',
+    //     visitInformations: 'Ceci est une description',
+    //     userId: 1, 
+    //     idEmployee: 2,
+    //     createdAt : new Date(),
+    //   }
+    // ], {});
   },
-  
+
   async down(queryInterface, Sequelize) {
     return queryInterface.bulkDelete('Agendas', null, {});
   }
-  };
+};
